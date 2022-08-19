1fe4fdfe631c6f356ae7cafdef972e52b48dfb13
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2022-08-19 11:04:05 -0300

    Testes find movies by genre: OK

---
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -0,0 +1,29 @@
+package com.devsuperior.movieflix.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieByGenreDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieResource {
+	
+	@Autowired
+	private MovieService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<MovieByGenreDTO>> findAllByGenre(
+			@RequestParam(value = "genreId", defaultValue = "0") Long genreId,
+			Pageable pageable) {
+		Page<MovieByGenreDTO> page = service.findAllByGenre(genreId, pageable);
+		return ResponseEntity.ok().body(page);
+	}
+}
